d05e7845e460cc49cea981c88310bc0f432a7178
caseyleigh54 <caseyleigh54@yahoo.com>
2025-07-31 15:59:11 -1000

    Fix: Use baseUrl for homepage redirect to work with GitHub Pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Redirect } from '@docusaurus/router';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 import React from 'react';
 
 export default function Home(): React.ReactElement {
-  return <Redirect to="/overview/introduction" />;
+  return <Redirect to={useBaseUrl('/overview/introduction')} />;
 } 
